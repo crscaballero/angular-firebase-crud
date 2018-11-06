b600c93f388428ee71466b3290fee75549cdb425
CrsCaballero <crsecaballero@gmail.com>
2018-11-05 23:46:02 -0300

    Terminado

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyCg-Bzi_B98rT4igOatQyVwC3tm_ymT_Aw",
+        authDomain: "angular-6-crud-26f0a.firebaseapp.com",
+        databaseURL: "https://angular-6-crud-26f0a.firebaseio.com",
+        projectId: "angular-6-crud-26f0a",
+        storageBucket: "angular-6-crud-26f0a.appspot.com",
+        messagingSenderId: "102562365609"
+    }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCg-Bzi_B98rT4igOatQyVwC3tm_ymT_Aw",
+        authDomain: "angular-6-crud-26f0a.firebaseapp.com",
+        databaseURL: "https://angular-6-crud-26f0a.firebaseio.com",
+        projectId: "angular-6-crud-26f0a",
+        storageBucket: "angular-6-crud-26f0a.appspot.com",
+        messagingSenderId: "102562365609"
+    }
+};
